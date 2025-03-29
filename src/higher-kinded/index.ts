d24f4e9d238893058f8842968e0d93236b912b8d
Mira <athes01@gmail.com>
2025-03-29 11:23:41 +0100

    add tsafe

--- src/higher-kinded/index.ts
+++ src/higher-kinded/index.ts
@@ -1,3 +1,4 @@
+import { assert, type Equals } from "tsafe";
 import type { ApplyHK, HK } from "./hk";
 
 type _NumberToString<T> = T extends number ? `${T}` : T;
@@ -25,3 +26,6 @@ const test_object = {
 } as const;
 
 type Test = MapProperties<typeof test_object, NumberToString>;
+
+// 1 is mapped to "1", the rest remains unchanged
+assert<Equals<Test, { readonly a: "1"; readonly b: "2"; readonly c: true }>>();
