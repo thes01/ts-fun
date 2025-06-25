0cbaa682773b39e7e7c35b7b306e158e6f4f0c64
Mira <athes01@gmail.com>
2025-06-25 07:54:03 +0200

    add fixed

---
+++ src/variance/fixed.ts
@@ -0,0 +1,29 @@
+interface Base {
+  foo: string;
+}
+
+interface Sub1 extends Base {
+  foo: "foo";
+}
+
+interface Sub2 extends Base {
+  foo: "bar";
+}
+
+type AnySub = Sub1 | Sub2;
+
+interface Context<T extends Base = Base> {
+  covariant: T;
+  contravariant: (x: T) => void;
+}
+
+// Distribute AnySub over Context:
+// DistributedContext = Context<Sub1> | Context<Sub2>
+type DistributedContext<T extends Base = AnySub> =
+  T extends infer A extends Base ? Context<A> : never;
+
+declare const sub_context: Context<Sub1>;
+
+declare function accept_base(context: DistributedContext): void;
+
+accept_base(sub_context);
