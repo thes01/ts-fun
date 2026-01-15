afdbfbd75647e3c9def140490bc087b316c0f2f3
Miroslav Sery <athes01@gmail.com>
2026-01-15 12:48:00 +0100

    comment

--- src/type-translating/expression_with_closure/expression.ts
+++ src/type-translating/expression_with_closure/expression.ts
@@ -1,7 +1,7 @@
 const expression = {
     content: 'Z + 10 * Y',
     variables: {
-        "Z": {}, // _ValueSource?_ Expression? Value?
-        "Y": {}, // _ValueSource?_ Expression? Value?
+        "Z": {}, // probably ValueSource
+        "Y": {}, // probably ValueSource
     }
 };
